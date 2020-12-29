74555ebecb2308eca426c36a8d7ca117e1757dbc
Saltpile123 <ikhoudvanberichten@gmail.com>
2020-12-29 01:22:25 +0100

    removed sealed from Abstractpages (without success)

--- App1/App1/NotWorking/AbstractPages.cs
+++ App1/App1/NotWorking/AbstractPages.cs
@@ -1,10 +1,10 @@
 ﻿namespace App1
 {
-    public sealed class AbstractPage1 : AbstractPage
+    public class AbstractPage1 : AbstractPage
     {
         public override string AbstractName => $"[{GetType().Name}] {base.AbstractName}";
     }
-    public sealed class AbstractPage2 : AbstractPage
+    public class AbstractPage2 : AbstractPage
     {
         public override string AbstractName => $"[{GetType().Name}] {base.AbstractName}";
     }
